79fcb1bd9fb7a32ca0e0af5f51d951315324af64
DebaPrasadbehura424 <DebaPrasadbehura424@gmail.com>
2025-03-24 00:11:18 +0530

    i

--- server/index.js
+++ server/index.js
@@ -6,27 +6,14 @@ require("dotenv").config();
 const app = express();
 const PORT = 5000;
 
-// const whitelist = ["https://portfolio-frontend-theta-sepia.vercel.app"];
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (whitelist.indexOf(origin) !== -1 || !origin) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("Not allowed by CORS"));
-//     }
-//   },
-//   methods: ["GET", "POST", "PUT", "DELETE"],
-//   allowedHeaders: ["Content-Type"],
-//   credentials: true,
-// };
-
-app.use(express.json());
 app.use(
   cors({
     origin: "https://portfolio-frontend-theta-sepia.vercel.app",
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
+app.use(express.json());
 
 mongoose
   .connect(
