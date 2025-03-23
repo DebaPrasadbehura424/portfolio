cf38a9dc9f5ebd2e68a245126e6d63aec3a67509
DebaPrasadbehura424 <DebaPrasadbehura424@gmail.com>
2025-03-23 21:07:13 +0530

    m

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import Hero from "../component/Hero";
 import Skills from "../component/Skills";
-import Footer from "../component/footer";
 function Home(props) {
   return (
     <div>
       <Hero />
       <Skills />
-      <Footer />
     </div>
   );
 }
